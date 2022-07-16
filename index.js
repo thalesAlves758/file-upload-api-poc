1723305d2c4cb5bb60d7272c39bf8592170c8b9a
Thales Alves <thalesalves758@gmail.com>
2022-07-16 18:50:10 -0300

    Use multer

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 import express from 'express';
 import cors from 'cors';
-import formidable from 'formidable';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
+import multer from 'multer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const UPLOAD_DIR = path.join(__dirname, 'uploads');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync(UPLOAD_DIR)) {
+      fs.mkdirSync(UPLOAD_DIR);
+    }
+
+    cb(null, UPLOAD_DIR);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/file', (req, res) => {
-  if (!fs.existsSync(UPLOAD_DIR)) {
-    fs.mkdirSync(UPLOAD_DIR);
-  }
-
-  const form = new formidable.IncomingForm({
-    uploadDir: UPLOAD_DIR,
-    keepExtensions: true,
-  });
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      res.status(500).send({ error: 'Erro ao salvar o arquivo' });
-    }
-
-    res.send('deu certo');
-  });
+app.post('/file', upload.single('file'), (req, res) => {
+  res.send(req.file);
 });
 
 app.listen(5000, () => {
